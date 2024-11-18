3dbd5927a6200c91ae1d240031682a7fcfe94687
unknown <anshuldhakate11@gmail.com>
2024-11-18 15:28:22 +0530

    done

--- controllers/authController.js
+++ controllers/authController.js
@@ -1,5 +1,6 @@
 const User = require('../models/User');
 const jwt = require('jsonwebtoken');
+const bcrypt = require('bcrypt');
 
 exports.registerUser = async (req, res) => {
   const { name, email, password } = req.body;
