53c88de7acd184ade17a76805218d0fa8cdd31a2
unknown <anshuldhakate11@gmail.com>
2024-11-18 16:46:48 +0530

    done

--- index.js
+++ index.js
@@ -6,11 +6,28 @@ const cors = require('cors');
 dotenv.config();
 const app = express();
 
-
 mongoose(); 
 
 app.use(express.json());
-app.use(cors());
+
+// Configure CORS
+const allowedOrigins = [
+  'http://localhost:3000', 
+  'https://mellow-salamander-b62922.netlify.app'
+];
+
+app.use(cors({
+  origin: function (origin, callback) {
+    // Allow requests with no origin (e.g., mobile apps or curl)
+    if (!origin) return callback(null, true);
+    if (allowedOrigins.includes(origin)) {
+      return callback(null, true);
+    } else {
+      return callback(new Error('Not allowed by CORS'));
+    }
+  },
+  credentials: true, // Allow credentials like cookies
+}));
 
 // Routes
 app.use('/api/auth', require('./routs/authRoutes'));
